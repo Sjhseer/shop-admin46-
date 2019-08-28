14a1706568c708963bdb602dfe3398a442cd81a0
Sjhseer <1315680458@qq.com>
2019-08-28 16:43:56 +0800

    首页提交[C

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>这是首页</div>
+  <div class="index">
+     <el-container class="index">
+    <el-header>
+      <div class="logo">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <div class="title">
+        <h1>电商后台管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临~ </span>
+        <a @click="logout" href="javascript:;">退出</a>
+      </div>
+    </el-header>
+
+    <el-container>
+  </div>
 </template>
 
 <script>
