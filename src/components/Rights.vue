3e45791e241294ea0a6353a1e92d24d146343c06
Sjhseer <1315680458@qq.com>
2019-08-30 15:05:25 +0800

    rights渲染

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>rights</div>
+  <div class="rights">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
 <script>
